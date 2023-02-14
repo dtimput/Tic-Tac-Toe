bc048deccae5d7590af9ba3d7fed4d882e81fabb
dtimput <122069647+dtimput@users.noreply.github.com>
2023-02-13 22:40:19 -0800

    Added Turn Logic and Player Objects

--- script.js
+++ script.js
@@ -1,35 +1,84 @@
-const gameElements = (function () {
+const Player = function (shape, isTurn, name, placements = []) {
+  return {
+    shape,
+    isTurn,
+    name,
+    placements,
+  };
+};
+
+const gameElements = (() => {
   const startMenu = document.querySelector(".start-menu");
   const gameBoard = document.querySelector(".game-board");
   const startGameButton = document.querySelector(".start-game");
   const xSelectorButton = document.querySelector(".x-selector");
   const oSelectorButton = document.querySelector(".o-selector");
-  let playerSelection = null;
+  const player = Player(null, null, null);
+  const opponent = Player(null, null, null);
 
   const load = () => {
     startGameButton.addEventListener("click", () => {
-      if (playerSelection !== null) {
+      if (player.shape !== null) {
         startMenu.style.display = "none";
         gameBoard.style.display = "grid";
       }
     });
     xSelectorButton.addEventListener("click", () => {
-      if (playerSelection !== "X") {
+      if (player.shape !== "X") {
         oSelectorButton.style.backgroundColor = "#a3abd8";
         xSelectorButton.style.backgroundColor = "#717797";
-        playerSelection = "X";
+        player.shape = "X";
+        player.isTurn = true;
+        opponent.shape = "O";
+        opponent.isTurn = false;
       }
     });
     oSelectorButton.addEventListener("click", () => {
-      if (playerSelection !== "O") {
+      if (player.shape !== "O") {
         xSelectorButton.style.backgroundColor = "#a3abd8";
         oSelectorButton.style.backgroundColor = "#717797";
-        playerSelection = "O";
+        player.shape = "O";
+        player.isTurn = false;
+        opponent.shape = "X";
+        opponent.isTurn = true;
       }
     });
   };
 
-  return { load, playerSelection };
+  return { load, player, opponent, gameBoard };
+})();
+
+const logicController = (() => {})();
+
+const boardElements = (() => {
+  const gameSquares = document.querySelectorAll(".box");
+
+  const gameBoard = {};
+
+  const loadBoard = () => {
+    gameSquares.forEach((boardElement) =>
+      boardElement.addEventListener("click", () => {
+        const square = boardElement;
+        if (gameElements.player.isTurn === true && square.textContent === "") {
+          square.textContent = gameElements.player.shape;
+          gameElements.player.placements.push(square.id);
+          gameElements.player.isTurn = false;
+          gameElements.opponent.isTurn = true;
+        } else if (
+          gameElements.opponent.isTurn === true &&
+          square.textContent === ""
+        ) {
+          square.textContent = gameElements.opponent.shape;
+          gameElements.opponent.placements.push(square.id);
+          gameElements.player.isTurn = true;
+          gameElements.opponent.isTurn = false;
+        }
+      })
+    );
+  };
+
+  return { loadBoard };
 })();
 
 gameElements.load();
+boardElements.loadBoard();
